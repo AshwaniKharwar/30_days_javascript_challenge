012cfeef071b506b1f04066e06de8f85ef1bd2a4
Ashwani Kharwar <ashwanikumarkharwar83@gmail.com>
2024-07-14 08:52:25 +0530

    day 2 challenge completed

---
+++ day2.js
@@ -0,0 +1,70 @@
+// Day 2: Operators
+
+// Activities:
+
+// Activity1: Arithmetic Opearators 
+
+// Task1: Write a program to add two numbers and log the result to the console . 
+let num1 = 24
+let num2= 35
+console.log(num1 + num2)
+
+// Task2: Write a program to subtract two numbers and log the result to the console . 
+let num3 = 35
+let num4 = 24
+console.log(num3 - num4)
+
+// Task3: Write a program to multiply two numbers and log the result to the console . 
+let num5 = 20
+console.log(num5 * num4)
+
+// Task4: Write a program to divide two numbers and log the result to the console . 
+let num6 = 5
+console.log(num6 / num5)
+
+// Task5: Write a program to find the remainder when one number is divided by another and log the result to the console . 
+console.log(num5 % num6)
+
+// Activity1: Assignment Opearators 
+
+// Task6: Use the += operator to add a number to a variable and log the result to the console. 
+let num7 = 21
+num7 += 3
+console.log(num7)
+
+// Task7: Use the -= operator to add a number to a variable and log the result to the console. 
+num7 -= 3
+console.log(num7)
+
+// Activity1: Comparison Opearators 
+
+// Task8: Write a program to compare two number using > ans < and log the result to the console. 
+console.log(num7 > num6)
+console.log(num7 < num6)
+
+// Task9: Write a program to compare two number using >= ans <= and log the result to the console. 
+let num8 = 21
+console.log(num8 >= num7)
+console.log(num8 <= num2)
+
+// Task10: Write a program to compare two number using == ans === and log the result to the console. 
+let num9 = "21"
+console.log(num9 == num8)
+console.log(num9 === num8)
+
+// Activity1: Logical Opearators 
+
+// Task11: Write a program that uses the && operator to combine two condition and log the result to the console. 
+console.log(num9 == num8 && num9 === num8)
+
+// Task12: Write a program that uses the && operator to combine two condition and log the result to the console. 
+console.log(num9 == num8 || num9 === num8)
+
+// Task13: Write a program that uses the ! operator to combine two condition and log the result to the console. 
+console.log(!(num8 == num9))
+
+// Activity1: Ternary Opearators 
+
+// Task14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console. 
+let num10 = -24
+console.log((num10 > 0) ? "positive" : "negative")
